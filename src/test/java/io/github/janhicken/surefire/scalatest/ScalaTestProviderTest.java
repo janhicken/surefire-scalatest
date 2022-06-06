c9c7fe6f6e7d5c26eb8856d9738df3fd61ac1d00
Jan Hicken <jan.hicken@ottogroup.com>
2022-06-06 17:46:51 +0200

    Ensure code is formatted with scalafmt and google-java-format

--- src/test/java/io/github/janhicken/surefire/scalatest/ScalaTestProviderTest.java
+++ src/test/java/io/github/janhicken/surefire/scalatest/ScalaTestProviderTest.java
@@ -1,5 +1,16 @@
 package io.github.janhicken.surefire.scalatest;
 
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.apache.maven.surefire.booter.BaseProviderFactory;
 import org.apache.maven.surefire.booter.ForkingReporterFactory;
 import org.apache.maven.surefire.providerapi.ProviderParameters;
@@ -13,83 +24,74 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.scalatest.tools.SurefireReporter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import static org.junit.Assert.*;
-
 public class ScalaTestProviderTest {
 
-    @Rule
-    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-    private ScalaTestProvider provider;
-    private ByteArrayOutputStream output;
+  private ScalaTestProvider provider;
+  private ByteArrayOutputStream output;
 
-    @Before
-    public void setUp() throws Exception {
-        this.output = new ByteArrayOutputStream();
-        final ProviderParameters providerParameters = new TestProviderParameters(
+  @Before
+  public void setUp() throws Exception {
+    this.output = new ByteArrayOutputStream();
+    final ProviderParameters providerParameters =
+        new TestProviderParameters(
             new PrintStream(output),
             Arrays.asList(
-                ScalaTestProvider.class, SurefireReporter.class,
-                ScalaTestProviderTest.class, ExampleSpec.class, HiddenSpec.class
-            ),
-            temporaryFolder.newFolder()
-        );
+                ScalaTestProvider.class,
+                SurefireReporter.class,
+                ScalaTestProviderTest.class,
+                ExampleSpec.class,
+                HiddenSpec.class),
+            temporaryFolder.newFolder());
 
-        provider = new ScalaTestProvider(providerParameters);
+    provider = new ScalaTestProvider(providerParameters);
+  }
+
+  @Test
+  public void test_findExampleSpec() {
+    final Iterable<Class<?>> suites = provider.getSuites();
+    final List<Class<?>> list =
+        StreamSupport.stream(suites.spliterator(), false).collect(Collectors.toList());
+    assertEquals("The discovery suites list must have exactly one entry", 1, list.size());
+    assertEquals(
+        "The discovery suites list must contain only the ExampleSpec",
+        ExampleSpec.class,
+        list.get(0));
+  }
+
+  @Test
+  public void test_run() throws UnsupportedEncodingException {
+    provider.invoke(null);
+
+    final String outputString = output.toString("UTF-8");
+    assertTrue(outputString.contains("- should do something without exception"));
+    assertTrue(outputString.contains("- should ignore something !!! IGNORED !!!"));
+    assertTrue(outputString.contains("- should fail dividing by 0 *** FAILED ***"));
+    assertFalse(outputString.contains("- not be discovered"));
+  }
+
+  static class TestProviderParameters extends BaseProviderFactory {
+
+    private final PrintStream printStream;
+
+    public TestProviderParameters(
+        final PrintStream printStream, final List<Class<?>> discoveredClasses, final File tmpDir) {
+      super(new ForkingReporterFactory(true, printStream), false);
+      this.printStream = printStream;
+
+      setClassLoaders(getClass().getClassLoader());
+      setProviderProperties(new HashMap<>());
+      setReporterConfiguration(new ReporterConfiguration(tmpDir, true));
+
+      final List<String> classNames =
+          discoveredClasses.stream().map(Class::getName).collect(Collectors.toList());
+      new DefaultScanResult(classNames).writeTo(getProviderProperties());
     }
 
-    @Test
-    public void test_findExampleSpec() {
-        final Iterable<Class<?>> suites = provider.getSuites();
-        final List<Class<?>> list = StreamSupport.stream(suites.spliterator(), false)
-            .collect(Collectors.toList());
-        assertEquals("The discovery suites list must have exactly one entry",
-            1, list.size());
-        assertEquals("The discovery suites list must contain only the ExampleSpec",
-            ExampleSpec.class, list.get(0));
+    @Override
+    public ConsoleStream getConsoleLogger() {
+      return new DefaultDirectConsoleReporter(printStream);
     }
-
-    @Test
-    public void test_run() throws UnsupportedEncodingException {
-        provider.invoke(null);
-
-        final String outputString = output.toString("UTF-8");
-        assertTrue(outputString.contains("- should do something without exception"));
-        assertTrue(outputString.contains("- should ignore something !!! IGNORED !!!"));
-        assertTrue(outputString.contains("- should fail dividing by 0 *** FAILED ***"));
-        assertFalse(outputString.contains("- not be discovered"));
-    }
-
-    static class TestProviderParameters extends BaseProviderFactory {
-
-        private final PrintStream printStream;
-
-        public TestProviderParameters(final PrintStream printStream, final List<Class<?>> discoveredClasses, final File tmpDir) {
-            super(new ForkingReporterFactory(true, printStream), false);
-            this.printStream = printStream;
-
-            setClassLoaders(getClass().getClassLoader());
-            setProviderProperties(new HashMap<>());
-            setReporterConfiguration(new ReporterConfiguration(tmpDir, true));
-
-            final List<String> classNames =
-                discoveredClasses.stream().map(Class::getName).collect(Collectors.toList());
-            new DefaultScanResult(classNames).writeTo(getProviderProperties());
-        }
-
-        @Override
-        public ConsoleStream getConsoleLogger() {
-            return new DefaultDirectConsoleReporter(printStream);
-        }
-    }
+  }
 }
